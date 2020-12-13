049ef9ddd683a03db5493a5a0752036293d52f41
Alenka <ms.alenchik@gmail.com>
2020-12-13 17:18:13 +0200

    homework 2

---
+++ 2.js
@@ -0,0 +1,14 @@
+// Дан образец JSON-строки:
+
+// {"name":"Anton","age":36,"skills":["Javascript","HTML","CSS"],"salary":80000}
+// Ваша задача — создать JS - объект, который при преобразовании в JSON будет возвращать в качестве результата
+// такую же JSON - строку, как в образце.Получившуюся строку вывести в консоль.
+
+
+const obj = JSON.parse(`{"name":"Anton","age":36,"skills":["Javascript","HTML","CSS"],"salary":80000}`);
+
+console.log(obj);
+
+const jsonString = JSON.stringify(obj);
+
+console.log(jsonString);
