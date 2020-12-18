4c1e7a25c5b3058729ab264b0789bc08cf6a3b53
Alenka <ms.alenchik@gmail.com>
2020-12-18 21:40:13 +0200

    homework 3

--- 2.js
+++ 2.js
@@ -5,7 +5,8 @@
 // такую же JSON - строку, как в образце.Получившуюся строку вывести в консоль.
 
 
-const obj = JSON.parse(`{"name":"Anton","age":36,"skills":["Javascript","HTML","CSS"],"salary":80000}`);
+const obj = JSON.parse(`{"name
+":"Anton","age":36,"skills":["Javascript","HTML","CSS"],"salary":80000}`);
 
 console.log(obj);
 
